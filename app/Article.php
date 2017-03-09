ed2c44ba04782cdbb71bd1b3ee4af02879045f97
zyten <sruban707@hotmail.com>
2017-03-09 18:53:51 +0800

    added some documentation using DocBlockr

--- app/Article.php
+++ app/Article.php
@@ -7,37 +7,62 @@ use Carbon\Carbon;
 
 class Article extends Model
 {
-	//These are allowed to be mass assigned using Article::create([]);
-	//If not declared, can only be inserted one by one $article-field = 'val'; $article->save();
+	/**
+	 * Fillable fields for and Article. (From a biz perspective)
+	 * e.g. Prevents client spoofing when we are using Article::create([$requet->all()]) without sanitation
+	 * @var array
+	 */
 	protected $fillable = [
 		'title',
 		'body',
 		'published_at'
 	];
 
-	//will be inserted as a Carbon date alongside created_at and updated_at
+	/**
+	 * Additional fields to treat as Carbon instance
+	 * e.g. Laravel defaults are created_at and updated_at
+	 * @var array
+	 */
 	protected $dates = [
 		'published_at'
 	];
-
-	//Query scopes = custom where clause that will be used a lot thoughout the app
+	
+	/**
+	 * Scope queries to articles that have been published
+	 * 
+	 * @param  $query
+	 */
 	public function scopePublished($query) 
 	{
 		$query->where('published_at', '<=', Carbon::now());
 	}	
 
+	/**
+	 * Scope queries to articles that have no been published
+	 * 
+	 * @param  $query
+	 */
 	public function scopeUnpublished($query) 
 	{
 		$query->where('published_at', '>', Carbon::now());
 	}
-	
-	//Accessors and Mutators
-	//Accessors = getters
-	//Mutators are used to modify fields before insertion
-	//e.g. setUserAddressAttribute() -- naming convention
+
+	/**
+	 * Set the published_at attribute
+	 * Mutate date from view (e.g. HTML formated) before db insertion
+	 * @param $date
+	 */
 	public function setPublishedAtAttribute($date) 
 	{
 		//$this->attributes['published_at'] = Carbon::createFromFormat('Y-m-d', $date); //stores current time
 		$this->attributes['published_at'] = Carbon::parse($date); //midnight
 	}
 }
+
+	//Query scopes = custom where clause that will be used a lot thoughout the app
+	//Convention = scopeName
+	
+	//Accessors and Mutators
+	//Accessors = getters
+	//Mutators are used to modify fields before insertion
+	//e.g. setUserAddressAttribute() -- naming convention
--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -10,6 +10,10 @@ use Illuminate\Http\Request;
 
 class ArticlesController extends Controller
 {
+    /**
+     * Display all articles
+     * @return Response
+     */
     public function index()
     {
     	$articles = Article::latest('published_at')->published()->orderBy('created_at', 'desc')->get(); //Article::order_by('published_at' => 'desc'); (default = created_at)
@@ -17,6 +21,11 @@ class ArticlesController extends Controller
     	return view('articles.index', compact('articles'));
     }
 
+    /**
+     * Display a single article
+     * @param  integer $id
+     * @return Response
+     */
     public function show($id) 
     {
     	$article = Article::findOrFail($id);
@@ -24,12 +33,20 @@ class ArticlesController extends Controller
     	return view('articles.show', compact('article'));
     }
 
+    /**
+     * Display form to create a new article
+     * @return Response
+     */
     public function create() 
     {
     	return view('articles.create');
     }
 
-    //Using Form Request - separate validation fron controller login
+    /**
+     * Save a new article
+     * @param  ArticleRequest $request Using Form Request to separate validation from controller logic
+     * @return Response                Redirect to Articles list
+     */
     public function store(ArticleRequest $request) 
     {
     	Article::create($request->all());
@@ -37,6 +54,11 @@ class ArticlesController extends Controller
     	return redirect('articles');
     }
 
+    /**
+     * Display form to edit an article
+     * @param  integer $id
+     * @return Response
+     */
     public function edit($id) 
     {
         $article = Article::findOrFail($id);
@@ -44,6 +66,12 @@ class ArticlesController extends Controller
         return view('articles.edit', compact('article'));
     }
 
+    /**
+     * Save updated article
+     * @param  integer         $id     
+     * @param  ArticleRequest $request
+     * @return Response               
+     */
     public function update($id, ArticleRequest $request) 
     {
         $article = Article::findOrFail($id);
@@ -52,6 +80,7 @@ class ArticlesController extends Controller
 
         return redirect('articles');
     }
+}
 
     //Validate within controller login - more feasible for basic stuff e.g. MPS should definitely us Form Request (logic itself is quite long)
     // public function store(\Illuminate\Http\Request $request) 
@@ -68,4 +97,3 @@ class ArticlesController extends Controller
 
     //     return redirect('articles');
     // }
-}
